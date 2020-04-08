2a213ad1bcf0d37be9d705a91c490dc5c19247e4
xanayax <x.anaya@sapalomera.cat>
2020-04-08 13:00:47 +0200

    Main - Hello WARUDO

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class Main {
     public static void main(String args) {
-        System.out.println("Hello GIT");
+        System.out.println("Hello WARUDO");
     }
 }
