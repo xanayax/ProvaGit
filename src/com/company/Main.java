d0ccf47ad909b6b9cdb3634fc0d955b276aaf7a9
xanayax <x.anaya@sapalomera.cat>
2020-04-08 13:13:07 +0200

    Hello alumne1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,6 @@ package com.company;
 public class Main {
     public static void main(String args) {
         System.out.println("Hello WARUDO");
+        System.out.println("Sóc l'alumne1");
     }
 }
